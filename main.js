e0aa0e16aab16f85d28e28a2f5136e712fc92145
Prakash7989 <prakashbhukya224@gmail.com>
2024-08-27 11:13:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.getElementById('nav-2').style.display = 'flex';
+});
+
+document.getElementById('close-icon').addEventListener('click', function() {
+    document.getElementById('nav-2').style.display = 'none';
+});
